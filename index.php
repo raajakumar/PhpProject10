8a85ac1f9f333ad8d46ac2fdaf8d9ff3cdc93c98
raajakumar <raajakumarmca@gmail.com>
2014-12-09 10:46:24 +0530

    commit

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+            
+        </title>
+    </head>
+    <body>
+     
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+<input type="radio" name="radio" value="Radio 1">Radio 1
+<input type="radio" name="radio" value="Radio 2">Radio 2
+<input type="radio" name="radio" value="Radio 3">Radio 3
+<input type="submit" name="submit" value="Get Selected Values" />
+</form>
+<?php
+if (isset($_POST['submit']))
+    {
+if(isset($_POST['radio']))
+{
+echo "You have selected :".$_POST['radio']; 
+}
+}
+?>
+      
+    </body>
+</html>
